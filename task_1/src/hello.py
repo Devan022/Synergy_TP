5e63ffe6127cfdc69fe77fecaced45481734bf40
Devansh Tiwari <tiwaridevansh289@gmail.com>
2026-06-24 23:25:51 +0530

    Initial Synergy Taskphase setup

---
+++ task_1/src/hello.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello from Task 1")
+    print("Python virtual environment is working correctly.")
+    print("Synergy Task Phase setup complete.")
+
+
+if __name__ == "__main__":
+    main()
